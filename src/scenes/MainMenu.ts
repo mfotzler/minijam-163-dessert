72077bb0ed904e2f359259bee8352e055d69faf3
Matthew Fotzler <mfotzler@gmail.com>
2024-07-21 09:23:35 -0500

    Remove blower-san assets and update title graphic

--- src/scenes/MainMenu.ts
+++ src/scenes/MainMenu.ts
@@ -34,8 +34,8 @@ export default class MainMenu extends BaseScene {
 			repeat: -1
 		});
 
-		let sprinkle1 = this.add.sprite(200, 90, 'textures', 'sprinkle1');
-		let sprinkle2 = this.add.sprite(this.renderer.width - 200, 90, 'textures', 'sprinkle1');
+		let sprinkle1 = this.add.sprite(150, 90, 'textures', 'sprinkle1');
+		let sprinkle2 = this.add.sprite(this.renderer.width - 150, 90, 'textures', 'sprinkle1');
 
 		sprinkle1.scale = 4;
 		sprinkle2.scale = 4;
